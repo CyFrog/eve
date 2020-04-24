097d484f4aedc799c8b376e2755e9a2aa7410bc4
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-04-24 16:25:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const EventSource = require("eventsource");
 //const btoa = require("btoa");
 const tokenId = process.env.BOT_TOKEN;
-
 const Discord = require('discord.js');
   const client = new Discord.Client();
   let envID = "";
+
+console.log("start);
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   });
