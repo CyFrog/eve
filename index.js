b90f1120a48f21b5fbc70e040a3ce7ea1547445b
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-05-04 13:46:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,22 +17,10 @@ client.on('ready', () => {
   });
 
 client.on('message', msg => {
-  let message=msg.content.toLowerCase(); let [input,dice]=message.split(" "); 
-//  if(input === "roll 1d6") { input="roll"; x=1; }
-//  if(input === "roll 2d6") { input="roll"; x=2; }
-//  if(input === "roll 3d6") { input="roll"; x=3; }
-//  if(input === "roll 4d6") { input="roll"; x=4; }
-//  if(input === "roll 5d6") { input="roll"; x=5; }
-//  if(input === "roll 6d6") { input="roll"; x=6; }
-//  if(input === "roll 7d6") { input="roll"; x=7; }
-//  if(input === "roll 8d6") { input="roll"; x=8; }
-//  if(input === "roll 9d6") { input="roll"; x=9; }
-//  if(input === "roll 10d6") { input="roll"; x=10; }
-//  if(input === "roll 11d6") { input="roll"; x=11; }
-//  if(input === "roll 12d6") { input="roll"; x=12; }
+  let message=msg.content.toLowerCase(); let [input,x]=message.split(" "); 
   if (input === 'roll') {
     let dieRoll=0; let amount=0; let diceText=""; let reply="";
-    for(i=0; i<dice; i++) { 
+    for(i=0; i<x; i++) { 
       dieRoll=parseInt((Math.random() * 6)+1);
       if(dieRoll==6) { }
       
@@ -40,7 +28,7 @@ client.on('message', msg => {
       amount+=dieRoll; }
     reply=diceText+" "+amount
     msg.reply(reply); 
-    dice=0; input="";
+    x=0; input="";
   }
   if (msg.content === 'roll') { msg.reply('pong'); }
 });
