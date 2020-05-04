7e9d2b0f1513cbaf4dcc1f983efb9074f42c7fbd
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-05-04 10:39:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,16 +17,29 @@ client.on('ready', () => {
   });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  let input=msg.content; let x=0;
+  if(input = "roll 1d6") { input="roll"; x=1; }
+  if(input = "roll 1d6") { input="roll"; x=2; }
+  if(input = "roll 1d6") { input="roll"; x=3; }
+  if(input = "roll 1d6") { input="roll"; x=4; }
+  if(input = "roll 1d6") { input="roll"; x=5; }
+  if(input = "roll 1d6") { input="roll"; x=6; }
+  if(input = "roll 1d6") { input="roll"; x=7; }
+  if(input = "roll 1d6") { input="roll"; x=8; }
+  if(input = "roll 1d6") { input="roll"; x=9; }
+  if(input = "roll 1d6") { input="roll"; x=10; }
+  if(input = "roll 1d6") { input="roll"; x=11; }
+  if(input = "roll 1d6") { input="roll"; x=12; }
+  if (input === 'roll') {
     let dieRoll=0; let amount=0; let diceText=""; let pong="";
-    for(i=0; i<3; i++) { 
+    for(i=0; i<x; i++) { 
       dieRoll=parseInt((Math.random() * 6)+1);
       diceText+=dice[dieRoll]+" ";
       amount+=dieRoll; }
     pong=diceText+" "+amount
     msg.reply(pong);
-   //  msg.reply('pong');
   }
+  if (msg.content === 'roll') { msg.reply('pong'); }
 });
 
 client.login(tokenId);
