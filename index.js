d79a46f06850b4919b2866548a5b80b362638ee3
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-05-05 12:19:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
 require("dotenv").config();
 
-// let token_obj = require(`token.json`); var token = token_obj["token"];
-// let owner_id_obj = require(`owner_discord_id.json`); var owner_discord_id = owner_id_obj["owner_discord_id"];
-
-// NPM MODULES
-
-// const commando = require("discord.js-commando");
-
-// const com = new commando.Client({ owner: owner_discord_id, commandPrefix: "-", 
-// disableEveryone: true, unknownCommandResponse: false });
-
-// const path = require("path");
-
-// let silvia_channel_id_obj = require(`silvia_channel_id.json`);
-// var silvia_channel_id = silvia_channel_id_obj["silvia_channel_id"];
-// var report_channel = require("report_channel.json");
-// var channel_id = report_channel["channel"];
-
-// const oneLine = require("common-tags").oneLine;
-// const sqlite = require("sqlite"); sqlite.open("./database.sqlite3");
-
-
-
 const tokenId = process.env.BOT_TOKEN;
   let dice=["🎲","⚀","⚁","⚂","⚃","⚄","⚅"]; 
-// ["⚀","⚁","⚂","⚃","⚄","⚅"]
-
-//const { transactionHandler } = require("./src/transactionHandler");
-// const EventSource = require("eventsource");
-//const btoa = require("btoa");
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -51,12 +24,13 @@ client.on('message', msg => {
       if(dieRoll==5) { stun+=1; } 
       if((dieRoll==1)||(dieRoll==2)) { glitch+=1; } }
     reply=diceText+" Total: "+amount + " High: "+high;
-    if(high>4) {
+    if(high>5) {
       if(crit>3) { reply+=", Annihilation! "+crit+ " critical successes"; }
       else if(crit==3) { reply+=", triple critical"; }
       else if(crit==2) { reply+=", double critical"; }
-      else if(crit==1) { reply+=", critical"; }
-      else if(stun>0) { reply+=", "+stun+" stun"; }
+      else if(crit==1) { reply+=", critical"; } }
+    if(high>4) {
+      if(stun>0) { reply+=", "+stun+" stun"; }
       if(glitch >= (x/2)) { reply+=", glitch"; } }
     else if(high>2) { 
       if(glitch >= (x/2)) { reply+=", glitch"; } 
