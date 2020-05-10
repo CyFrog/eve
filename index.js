66b21bf4088b9111fb2c8345d148af411ff67e8f
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-05-10 15:40:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 // const Telegraf = require("telegraf");
 // const session = require("telegraf/session");
-const { textHandler } = require("./src/handlers/textHandler");
+// const { textHandler } = require("./src/handlers/textHandler");
 
 const tokenId = process.env.BOT_TOKEN;
   let dice=["🎲","⚀","⚁","⚂","⚃","⚄","⚅"]; 
